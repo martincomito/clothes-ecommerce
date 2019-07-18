94af8f7fa0626feba137f27f7314bcaa20e7f828
Martín Comito <comito.martin@gmail.com>
2019-07-18 17:33:49 -0300

    Minor style fixes

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -52,7 +52,7 @@ class SignUp extends React.Component {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
       <div className='sign-up'>
-        <h2 className='title'>I do not have an account</h2>
+        <h1 className='title'>I do not have an account</h1>
         <span>Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
           <FormInput
