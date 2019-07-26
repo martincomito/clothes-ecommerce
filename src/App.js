0cb7cdb34606adc2a571b73514998f2f64feca2d
Martín Comito <comito.martin@gmail.com>
2019-07-26 15:31:05 -0300

    Added route for CheckoutPage component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import Header from './components/header/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 import './App.css';
-import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsuscribeFromAuth = null;
@@ -45,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
